3e8d3ab2ca3d03c9f8654cbd47dfe74c1179cbac
okachi2177-ship-it <okachi.2177@gmail.com>
2026-02-27 17:44:09 +0900

    Set canonical domain for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ const notoSansJP = Noto_Sans_JP({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://reigaku.online"),
   title: {
     default: "零学マネジメント | 偏った思考を零ポイントに戻す学問",
     template: "%s | 零学マネジメント",
